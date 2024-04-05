3d524a5fc7c150fc168bd6044e5b3b099e69db63
André ldev <andre_jp97@hotmail.com>
2024-04-05 02:06:16 -0300

    Tips C# EntidadeCPF

---
+++ C#/EntidadeCpf/Program.cs
@@ -0,0 +1,65 @@
+﻿
+try
+{
+	var cpfClientes = new List<Cpf>();
+	cpfClientes.Add("540.297.530-27");
+	cpfClientes.Add("867.758.450-13");
+} catch (Exception ex)
+{
+	Console.WriteLine(ex.ToString());
+}
+
+
+public class Cpf
+{
+	private string Cpf_;
+	public Cpf(string cpf)
+	{
+		Cpf_ = cpf;
+	}
+
+	public static implicit operator Cpf(string cpf)
+	{
+		if (!CpfValido(cpf))
+			throw new Exception($@"Cpf ""{cpf}"" é inválido");
+
+		return new Cpf(cpf);
+	}
+
+	public static implicit operator string(Cpf cpf) => cpf.Cpf_;
+
+	static bool CpfValido(string cpf)
+	{
+		int[] multiplicador1 = [10, 9, 8, 7, 6, 5, 4, 3, 2];
+		int[] multiplicador2 = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+		string tempCpf, digito;
+		int soma, resto;
+
+		cpf = cpf.Trim().Replace(".", "").Replace("-", "");
+		if (cpf.Length != 11)
+			return false;
+		tempCpf = cpf[..9];
+		soma = 0;
+
+		for (int i = 0; i < 9; i++)
+			soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+		resto = soma % 11;
+		if (resto < 2)
+			resto = 0;
+		else
+			resto = 11 - resto;
+		digito = resto.ToString();
+		tempCpf += digito;
+		soma = 0;
+		for (int i = 0; i < 10; i++)
+			soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+		resto = soma % 11;
+		if (resto < 2)
+			resto = 0;
+		else
+			resto = 11 - resto;
+		digito += resto.ToString();
+		return cpf.EndsWith(digito);
+	}
+}
+
---
+++ C#/EntidadeCpf/README.md
@@ -0,0 +1,5 @@
+
+# Entidade CPF
+
+Esse repositório faz parte de um post publicado no [linkedin](linkedin), que pode ser encontrado [aqui](aqui).
+
