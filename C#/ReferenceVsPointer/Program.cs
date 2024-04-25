d9c039b60c0ae9c8e002acc42617ef74044d1421
André ldev <andre_jp97@hotmail.com>
2024-04-25 01:56:47 -0300

    (C#) Reference vs Pointer

---
+++ C#/ReferenceVsPointer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+
+namespace ReferenceVsPointer;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		CodeSafe();
+		CodeUnsafe();
+	}
+
+	public static void CodeSafe()
+	{
+		Console.WriteLine($"{MethodBase.GetCurrentMethod()?.Name} - Without references");
+		var myStruct = new MyStruct(20);
+		var myAnotherStruct = myStruct;
+		myAnotherStruct.Age = 26;
+
+		Console.WriteLine(myStruct.Age);
+		Console.WriteLine(myAnotherStruct.Age);
+
+		Console.WriteLine(string.Concat(Enumerable.Repeat("-", 15)));
+	}
+
+	public unsafe static void CodeUnsafe()
+	{
+		Console.WriteLine($"{MethodBase.GetCurrentMethod()?.Name} - With references");
+		var myStruct = new MyStruct(20);
+		MyStruct* myStructPtr = &myStruct;
+		myStructPtr->Age = 26;
+
+		Console.WriteLine(myStruct.Age);
+		Console.WriteLine(myStructPtr->Age);
+
+		Console.WriteLine(string.Concat(Enumerable.Repeat("-", 15)));
+	}
+
+	public struct MyStruct(int age)
+	{
+		public int Age = age;
+	}
+}
---
+++ C#/ReferenceVsPointer/README.md
@@ -0,0 +1,6 @@
+
+# Reference vs Pointer
+
+Esse é um projeto baseado em um post no linkedin, é possível encontra-lo aqui: [✅ Reference vs Pointer]()
+
+Esse é um projeto estremamente simple, que visa apenas explanar a diferença entre pointer e reference.
