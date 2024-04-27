78c2786282ab7cc832ea0fe5901a368f769296eb
André ldev <andre_jp97@hotmail.com>
2024-04-27 18:35:01 -0300

    (C#) Regex Label

---
+++ C#/RegexLabel/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RegexLabel;
+
+using System;
+using System.Text.RegularExpressions;
+
+public static class RegexLabel
+{
+	public static void Main()
+	{
+		const string pattern = @"(?<tipo>\S+)(\s+|)((?<parcelas>\d)|)";
+		const string input = "Débito";
+		var tipo = "";
+		var parcelas = 0;
+
+		var m = Regex.Match(input, pattern);
+
+		tipo = m.Groups["tipo"].Value;
+		int.TryParse(m.Groups["parcelas"].Value, out parcelas);
+
+
+		Console.WriteLine($"tipo: {tipo} parcelas: {parcelas}");
+	}
+}
---
+++ C#/RegexLabel/README.md
@@ -0,0 +1,6 @@
+
+# Regex Label
+
+Esse é um projeto baseado em um post no linkedin, é possível encontra-lo aqui: [✅ Regex Label]()
+
+Projeto com exemplo simples de como nomear um grupo em regex.
